9e8c8361c0d33cce457749bc8a855a93c28291e2
Jenry560 <115893645+Jenry560@users.noreply.github.com>
2025-04-15 09:02:32 -0400

    fix test

--- test_index.py
+++ test_index.py
@@ -1,4 +1,4 @@
 def test_hola_mundo():
     with open("src/index.html", encoding="utf-8") as f:
         contenido = f.read()
-    assert "<h1>Hola Mundo</h1>" in contenido
+    assert "<h1>¡Hola Mundo!/h1>" in contenido
